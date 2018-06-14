72680c4c336a031014550fe483f5e134dbf163b9
João Vitor P <jvpereirafaustino@gmail.com>
2018-06-14 00:12:23 -0300

    update controllers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\admin\Post;
-use App\Models\admin\Category;
+use App\Models\Admin\Post;
+use App\Models\Admin\Category;
 
 class HomeController extends Controller
 {
